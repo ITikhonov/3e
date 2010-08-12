ded76abf5a052adf46febda23bc09ee0657361ed
kef <kefeer@brokestream.com>
2010-08-13 01:56:20 +0600

    ctrl for adding point

--- 3e.c
+++ 3e.c
@@ -48,15 +48,17 @@ int main() {
 			}
 			if(e.type==SDL_MOUSEBUTTONDOWN) {
 				if(e.button.button==SDL_BUTTON_LEFT) {
-					struct point *p=point+pointn++;
+					if(SDL_GetModState()&KMOD_CTRL) {
+						struct point *p=point+pointn++;
 
-					float x0 = e.button.x-sw()/2;
-					float y0 = e.button.y-sh()/2;
-					x0/=scale;
-					y0/=scale;
+						float x0 = e.button.x-sw()/2;
+						float y0 = e.button.y-sh()/2;
+						x0/=scale;
+						y0/=scale;
 
-					rot(y0,0,&p->y,&p->z,-rot_x);
-					rot(x0,p->z,&p->x,&p->z,-rot_y);
+						rot(y0,0,&p->y,&p->z,-rot_x);
+						rot(x0,p->z,&p->x,&p->z,-rot_y);
+					}
 				}
 				if(e.button.button==SDL_BUTTON_WHEELDOWN) { scale/=1.5; }
 				if(e.button.button==SDL_BUTTON_WHEELUP) { scale*=1.5; }
