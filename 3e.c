74dd1c71517b3b51171d22d0f28fd56529647170
kef <kefeer@brokestream.com>
2010-08-16 17:24:03 +0600

    clean up software rendering

--- 3e.c
+++ 3e.c
@@ -16,7 +16,7 @@
 SDL_Surface *screen, *rscreen;
 
 struct point {
-	GLint x,y,z,sx,sy,sz,sel;
+	GLint x,y,z,sel;
 } point[102400];
 
 struct tri {
@@ -34,22 +34,8 @@ float scale=1/100000.0;
 
 int style=2;
 
-float zbuf[800][600];
-
 int sw() { return 800; }
 int sh() { return 600; }
-
-void select_point(int x,int y) {
-	int i,z=INT_MIN,n=-1;
-	for(i=0;i<pointn;i++) {
-		struct point *p=point+i;
-		if(abs(x-p->sx)<3 && abs(y-p->sy)<3 && p->sz>z) {
-			n=i; z=p->sz;
-		}
-	}
-
-	point[n].sel=!point[n].sel;
-}
 
 void rot(int x0, int y0, int *x, int *y, float a) {
 	*x=x0*cos(a)+y0*sin(a);
@@ -64,96 +50,21 @@ void transform(int x0,int y0,int z0,int *x, int *y, int *z) {
 	*z*=scale;
 }
 
-void draw_span_line(int x0,int x1,int y,float z0,float z1,Uint32 color) {
-	int x;
-	int dx=x0<x1?1:-1;
-	float dz=(z1-z0)/(x1-x0);
-	for(x=x0;x!=x1;x+=dx) {
-		if(z0>zbuf[x][y]) {
-			pixelColor(screen,x,y,color);
-			//pixelColor(screen,x,y,((int)(128*(z0+300)/600)<<8)|0xff);
-			zbuf[x][y]=z0;
-		}
-		z0+=dz;
-	}
-}
-
-void draw_span(struct point *f, struct point *m, struct point *l, Uint32 color) {
-	float dx0=(l->sx-f->sx)/(float)(l->sy-f->sy);
-	float x0=f->sx;
-
-	float dz0=(l->sz-f->sz)/(float)(l->sy-f->sy);
-	float z0=f->sz;
-	if(f->sy!=m->sy) {
-		int y=f->sy;
-
-		float dx1=(m->sx-f->sx)/(float)(m->sy-f->sy);
-		float x1=f->sx;
-
-		float dz1=(m->sz-f->sz)/(float)(m->sy-f->sy);
-		float z1=f->sz;
-
-		for(;y<m->sy;y++) {
-			draw_span_line(x0,x1,y,z0,z1,color);
-			pixelColor(screen,x0,y,0xFF0000FF);
-			pixelColor(screen,x1,y,0x0000FFFF);
-			x0+=dx0;
-			x1+=dx1;
-			z0+=dz0;
-			z1+=dz1;
+void select_point(int x,int y) {
+	int i,z=INT_MIN,n=-1;
+	for(i=0;i<pointn;i++) {
+		struct point *p=point+i;
+		int sx,sy,sz;
+		transform(p->x,p->y,p->z,&sx,&sy,&sz);
+		if(abs(x-sx)<3 && abs(y-sy)<3 && sz>z) {
+			n=i; z=sz;
 		}
 	}
 
-	if(m->sy!=l->sy) {
-		int y=m->sy;
-
-		float dx1=(l->sx-m->sx)/(float)(l->sy-m->sy);
-		float x1=m->sx;
-
-		float dz1=(l->sz-m->sz)/(float)(l->sy-m->sy);
-		float z1=m->sz;
-
-		for(;y<l->sy;y++) {
-			draw_span_line(x0,x1,y,z0,z1,color);
-			pixelColor(screen,x0,y,0xFF0000FF);
-			pixelColor(screen,x1,y,0x0000FFFF);
-			x0+=dx0;
-			x1+=dx1;
-			z0+=dz0;
-			z1+=dz1;
-		}
-	}
+	if(n>=0) point[n].sel=!point[n].sel;
+	printf("click: %d\n",n);
 }
 
-
-void draw_tri(int v1, int v2, int v3, Uint32 color) {
-	struct point *a=point+v1, *b=point+v2, *c=point+v3;
-	struct point *f,*l,*m;
-
-	int ab = a->sy < b->sy;
-	int bc = b->sy < c->sy;
-	int ac = a->sy < c->sy;
-
-	if(ab) {
-		if(bc) { f=a;m=b;l=c; }
-		else { if(ac) { f=a;m=c;l=b; } else { f=c;m=a;l=b;} }
-	} else {
-		if(!bc) { f=c;m=b;l=a; }
-		else { if(ac) { f=b;m=a;l=c; } else { f=b;m=c;l=a;} }
-	}
-
-	assert(f->sy<=m->sy);
-	assert(m->sy<=l->sy);
-
-	draw_span(f,m,l,color);
-}
-
-void draw_tri_wire(int v1, int v2, int v3) {
-	struct point *a=point+v1, *b=point+v2, *c=point+v3;
-	aalineColor(screen,a->sx,a->sy,b->sx,b->sy,0xCCCCCCFF);
-	aalineColor(screen,b->sx,b->sy,c->sx,c->sy,0xCCCCCCFF);
-	aalineColor(screen,c->sx,c->sy,a->sx,a->sy,0xCCCCCCFF);
-}
 
 int mx,my,mz;
 
@@ -283,8 +194,6 @@ int main() {
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 	rscreen=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE|SDL_OPENGLBLIT);
-	screen=SDL_CreateRGBSurface(SDL_SWSURFACE,800,600,32,rscreen->format->Rmask,rscreen->format->Gmask,
-					rscreen->format->Bmask,rscreen->format->Amask);
 	
 	initgl();
 
@@ -336,7 +245,6 @@ int main() {
 				}
 				if(e.button.button==SDL_BUTTON_WHEELDOWN) { scale/=1.5; }
 				if(e.button.button==SDL_BUTTON_WHEELUP) { scale*=1.5; }
-				
 			}
 			if(e.type==SDL_MOUSEMOTION) {
 				if(SDL_GetModState()&KMOD_SHIFT) {
@@ -355,91 +263,9 @@ int main() {
 		}
 
 
-		SDL_FillRect(screen,0,0xffffffff);
-		int i;
-
-		for(i=0;i<800*600;i++) {
-			*(((float*)zbuf)+i)=-INFINITY;
-		}
-
-		if(style==1) {
-			for(i=0;i<trin;i++) {
-				draw_tri(tri[i].v[0],tri[i].v[1],tri[i].v[2], (0xFF<<(8+i*2))|0xFF );
-			}
-		} else if(style==2) {
-			for(i=0;i<trin;i++) {
-				draw_tri_wire(tri[i].v[0],tri[i].v[1],tri[i].v[2]);
-			}
-		}
-
-		
-		int minz1=minz,spanz=maxz-minz;
-		maxz=INT_MIN; minz=INT_MAX;
-		for(i=0;i<pointn;i++) {
-			struct point *p=point+i;
-
-			int x,y,z; transform(p->x,p->y,p->z,&x,&y,&z);
-
-			x+=sw()/2;
-			y+=sh()/2;
-
-			p->sx=x; p->sy=y; p->sz=z;
-			if(z<minz) minz=z; if(z>maxz) maxz=z;
-
-			float c=(z-minz1)/(float)spanz;
-			unsigned int color=0xff-0xff*c;
-			color=(color<<16)|(color<<8)|color;
-
-			SDL_Rect dh={x,y-2,1,5};
-			SDL_FillRect(screen,&dh,color);
-			SDL_Rect dv={x-2,y,5,1};
-			SDL_FillRect(screen,&dv,color);
-			if(p->sel) {
-				SDL_Rect ds={x-2,y-2,5,5};
-				SDL_FillRect(screen,&ds,0xAAAAAA);
-
-				int nx,ny,nz;
-
-#if 0
-				transform(p->x,p->y+10,p->z,&nx,&ny,&nz);
-				aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0xFF0000FF);
-
-				transform(p->x+10,p->y,p->z,&nx,&ny,&nz);
-				aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0x00FF00FF);
-
-				transform(p->x,p->y,p->z+10,&nx,&ny,&nz);
-				aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0x0000FFFF);
-#endif
-
-				if(mx||my||mz) {
-					transform(p->x+mx,p->y+my,p->z+mz,&nx,&ny,&nz);
-					int rx=nx+sw()/2,ry=ny+sh()/2;
-
-					SDL_Rect ds={nx+sw()/2-2,ny+sh()/2-2,5,5};
-					SDL_FillRect(screen,&ds,0xFF0000);
-					aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0xAAAAAAFF);
-
-					transform(p->x+mx,p->y,p->z,&nx,&ny,&nz);
-					aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0xFF0000FF);
-
-					transform(p->x,p->y+my,p->z,&nx,&ny,&nz);
-					aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0x00FF00FF);
-
-					transform(p->x,p->y,p->z+mz,&nx,&ny,&nz);
-					aalineColor(screen,x,y,nx+sw()/2,ny+sh()/2,0x0000FFFF);
-				}
-			}
-		}
-
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
-		//SDL_BlitSurface(screen,0,rscreen,0);
-		//SDL_UpdateRect(rscreen,0,0,0,0);
-
 		gldraw();
 		SDL_GL_SwapBuffers();
-		//SDL_Flip(rscreen);
-
 	}
 end:	SDL_Quit();
 }
